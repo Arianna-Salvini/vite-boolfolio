f78a9df581284343819bad4c2353c3365982ef40
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-05-30 16:47:12 +0200

    Install axios

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,36 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
+import axios from 'axios';
+
+export default {
+    name: 'App',
+    data() {
+        return {
+            url_API: 'http://127.0.0.1:8000/',
+            url_projects: 'admin/projects',
+            projects: [],
+            loading: true
+        }
+    },
+    methods: {
+        callAPI(url) {
+            axios
+                .get(url)
+                .then(response => {
+                    console.log(response);
+                    this.projects = response.data.projects
+                    this.loading = false
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+        }
+    },
+
+    mounted() {
+
+    }
+}
 </script>
 
 <template>
