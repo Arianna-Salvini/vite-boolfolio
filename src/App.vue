50765de2d354ebef9f667599f64b714e952ed67e
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-05-30 18:42:04 +0200

    Import components

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,28 @@
 
 import axios from 'axios';
 
+import AppHeader from "./components/layouts/AppHeader.vue";
+import AppMain from "./components/layouts/AppMain.vue";
+import AppFooter from "./components/layouts/AppFooter.vue";
+
 export default {
     name: 'App',
+
+    components: {
+        AppFooter,
+        AppHeader,
+        AppMain,
+    },
+
     data() {
         return {
             url_API: 'http://127.0.0.1:8000/',
-            url_projects: 'admin/projects',
+            url_projects: 'api/projects',
             projects: [],
-            loading: true
+            loading: true,
         }
     },
+
     methods: {
 
         callAPI(url) {
@@ -29,13 +41,21 @@ export default {
     },
 
     mounted() {
-
+        this.callAPI(this.url_API + this.url_projects);
     }
 }
 </script>
 
 <template>
+    <div>
 
+        <AppHeader />
+
+        <AppMain :projects="projects" :loading="loading" />
+
+        <AppFooter />
+
+    </div>
 </template>
 
 <style></style>
