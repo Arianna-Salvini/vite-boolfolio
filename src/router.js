f21262bcc6667f2741eddfb62c908b9d3bae707a
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-05-31 18:04:11 +0200

    Add router

---
+++ src/router.js
@@ -0,0 +1,38 @@
+import { createWebHistory, createRouter } from 'vue-router'
+
+
+import AppHome from "./components/views/AppHome.vue";
+import AppAbout from "./components/views/AppAbout.vue";
+import AppProjects from './components/views/AppProjects.vue';
+import AppContacts from "./components/views/AppContacts.vue";
+
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: AppHome
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: AppAbout
+        },
+        {
+            path: '/projects',
+            name: 'projects',
+            component: AppProjects
+        },
+        {
+            path: '/contacts',
+            name: 'contacts',
+            component: AppContacts
+        }
+    ]
+})
+
+
+export default router;
