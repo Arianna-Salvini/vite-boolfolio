a43bd0a64d25c3d660d0b93b1bc119efe7e3f2ea
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-06-04 12:15:28 +0200

    Add some general style

--- src/App.vue
+++ src/App.vue
@@ -14,20 +14,36 @@ export default {
 </script>
 
 <template>
-    <div container>
 
-        <AppHeader />
 
+    <AppHeader />
+    <div class="body_main">
         <RouterView />
-
-        <AppFooter />
-
     </div>
+
+    <AppFooter />
+
+
 </template>
 
 <style>
-.app {
-    min-width: 940px;
-    max-width: 1440px;
+.body_main {
+    height: calc(100vh - 4rem);
+    /* background-color: #011517; */
+    background: linear-gradient(45deg, #02202a, #003344, #003f5a, #00485a, #00505a, #004853, #043339, #011517);
+    background-size: 200%;
+    background-position: left;
+    animation: bg_animation 10s infinite alternate;
+}
+
+@keyframes bg_animation {
+    0% {
+        background-position: left;
+    }
+
+    100% {
+        background-position: right;
+    }
+
 }
 </style>
