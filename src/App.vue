2b861a4237946945c56cbc86531f8415364abd34
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-06-04 17:57:35 +0200

    Add utilities class for neon text Fix background colors and animation time

--- src/App.vue
+++ src/App.vue
@@ -30,10 +30,10 @@ export default {
 .body_main {
     height: calc(100vh - 4rem);
     /* background-color: #011517; */
-    background: linear-gradient(45deg, #02202a, #003344, #003f5a, #00485a, #00505a, #004853, #043339, #011517);
+    background: linear-gradient(45deg, #001823, #002f43, #003f5a, #00485a, #00505a, #004853, #043339, #011517);
     background-size: 200%;
     background-position: left;
-    animation: bg_animation 10s infinite alternate;
+    animation: bg_animation 9s alternate infinite;
 }
 
 @keyframes bg_animation {
@@ -45,5 +45,30 @@ export default {
         background-position: right;
     }
 
+}
+
+/* Utilities */
+.text_neon {
+    color: #96f0fe;
+    text-shadow: 0 0 3px #00505a,
+        0 0 5px #00acc6,
+        0 0 5px #00acc6,
+        0 0 10px #00acc6,
+        0 0 15px #00acc6,
+        0 0 30px #00acc6,
+        0 0 60px #00acc6,
+        0 0 120px #00acc6;
+
+    &:hover {
+        color: #b9f6ff;
+        text-shadow: 0 0 3px #006b79,
+            0 0 5px #00ddff,
+            0 0 10px #00ddff,
+            0 0 15px #00ddff,
+            0 0 30px #00ddff,
+            0 0 60px #00ddff,
+            0 0 120px #00ddff;
+        cursor: auto;
+    }
 }
 </style>
