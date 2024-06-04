3d1b98956eb8b02752227d2fb1371218608e84d4
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-06-04 17:56:28 +0200

    Edit style of home page

--- src/components/views/AppHome.vue
+++ src/components/views/AppHome.vue
@@ -1,11 +1,20 @@
 <script>
+// import ButtonGeneral from '../elements/ButtonGeneral.vue';
+
+// export default {
+//     name: 'AppHome',
+//     components: {
+//         ButtonGeneral,
+//     }
+
+// }
 </script>
 
 <template>
     <section class="body_main d-flex align-items-center">
         <div class="container d-flex align-items-center">
             <div class="left">
-                <h1>
+                <h1 class="text_neon">
                     &lt;Arianna Salvini /&gt;
                 </h1>
                 <h3>Full-stack Web Developer</h3>
@@ -15,10 +24,16 @@
                     adipisci
                     possimus vitae veniam earum.</p>
             </div>
+
             <div class="right">
                 <!-- image here -->
                 <div> Image</div>
             </div>
+        </div>
+        <div>
+            <!-- <ButtonGeneral />
+            <ButtonGeneral />
+            <ButtonGeneral /> -->
         </div>
     </section>
 </template>
@@ -32,12 +47,21 @@
 .right {
     width: 50%;
     height: 100%;
+
+    h3 {
+        text-shadow: 0 3px 7px #00232f;
+    }
+
+    p {
+        color: #00acc6;
+        text-shadow: 0 2px 10px #00232f;
+
+    }
 }
 
 .left {
     text-align: right;
     padding-right: 1rem;
-
 }
 
 .right {
