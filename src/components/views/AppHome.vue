c5a1117bf80b2a7fd662b293db51d8314870b102
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-06-04 12:15:40 +0200

    Edit homepage

--- src/components/views/AppHome.vue
+++ src/components/views/AppHome.vue
@@ -2,7 +2,46 @@
 </script>
 
 <template>
-
+    <section class="body_main d-flex align-items-center">
+        <div class="container d-flex align-items-center">
+            <div class="left">
+                <h1>
+                    &lt;Arianna Salvini /&gt;
+                </h1>
+                <h3>Full-stack Web Developer</h3>
+                <p class="pt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis nostrum a
+                    deleniti qui
+                    obcaecati amet
+                    adipisci
+                    possimus vitae veniam earum.</p>
+            </div>
+            <div class="right">
+                <!-- image here -->
+                <div> Image</div>
+            </div>
+        </div>
+    </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+.body_main {
+    height: 100%;
+}
+
+.left,
+.right {
+    width: 50%;
+    height: 100%;
+}
+
+.left {
+    text-align: right;
+    padding-right: 1rem;
+
+}
+
+.right {
+    text-align: left;
+    padding-left: 1rem;
+}
+</style>
